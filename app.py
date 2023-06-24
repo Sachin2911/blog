3ba3fc5d6206cf8797b167c18a08e4bf5cda96ad
Sachin2911 <Sachinmohan29@gmail.com>
2023-06-24 14:14:58 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import openai
+
+app = Flask(__name__)
+
+# Set up your OpenAI API key
+openai.api_key = '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'
+
+# Define the home route
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/generate_ideas', methods=['POST'])
+def generate_ideas():
+
+    category = request.form['category']
+    user_prompt = request.form['prompt']
+    prompt = f"Generate a sample {category} from the user-input: {user_prompt}"
+    print(prompt)
+    # Call the OpenAI API to generate ideas
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=100,
+        n=2,
+        stop=None,
+        temperature=0.2
+    )
+    print(response)
+    # Extract the generated ideas from the API response
+    ideas = [choice['text'].strip() for choice in response.choices]
+
+    # Render the results template with the generated ideas
+    return render_template('results.html', ideas=ideas)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
